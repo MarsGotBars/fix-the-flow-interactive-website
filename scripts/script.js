27ae137ea26dd12eef9bb7e97356323459d35b49
Marcin Magdziak <marcinm@live.nl>
2024-12-04 10:55:32 +0100

    initial commit
    first commit in this repo!
    also added view transition in css

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const pageWithoutScroll = window.innerWidth - 15;
+const navItems = document.querySelector(".nav-items");
+const nav = document.querySelector("nav");
+const burger = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+let isScrolled = false;
+
+// switch to intersectionObserver
+const checkScroll = () => {
+  const scrolledDown = window.scrollY > 0;
+
+  if (scrolledDown && !isScrolled) {
+    nav.classList.add("scrolled");
+    isScrolled = true;
+  } else if (!scrolledDown && isScrolled) {
+    nav.classList.remove("scrolled");
+    isScrolled = false;
+  }
+};
+
+pageWithoutScroll > 660 && window.addEventListener("scroll", checkScroll) && burgerMenu.addEventListener("click", OpenBurger);
+
+const OpenBurger = (e) => {
+  console.log(navItems);
+  e.preventDefault();
+  overlay.classList.toggle("open");
+  navItems.classList.toggle("open");
+};
+burger.addEventListener("click", OpenBurger);
+
