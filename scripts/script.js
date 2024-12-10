3ac8673398bee2fd2a957deaa9f5bb286bedc338
Marcin Magdziak <marcinm@live.nl>
2024-12-10 16:53:20 +0100

    changed publication list animation
    made it more simple whilst integrating js, will try some of Krijns' suggestions later on.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 const pageWithoutScroll = window.innerWidth - 15;
 const navItems = document.querySelector(".nav-items");
 const nav = document.querySelector("nav");
-const header = document.querySelector("header")
+const header = document.querySelector("header");
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay");
-const publications = document.querySelector(".pub-list")
+const publications = document.querySelector(".pub-list");
+let pubItems = document.querySelectorAll(".pub-list>li:not(.displayed)");
 let isScrolled = false;
 
 const OpenBurger = (e) => {
@@ -20,32 +21,33 @@ const handler = (entries) => {
   // because that's my nav node.
   // Here observe whether or not that node is in the viewport
   if (!entries[0].isIntersecting) {
-    nav.classList.add('scrolled')
+    nav.classList.add("scrolled");
   } else {
-    nav.classList.remove('scrolled')
+    nav.classList.remove("scrolled");
   }
-}
+};
 const options = {
   root: document.body, // The root of the observer
-  rootMargin: '0px 0px -99% 0px', // little hack to push the rootMargin up to the top of the page
-  threshold: 0 // how much % of the document.body should be visible for the handler function to fire?
+  rootMargin: "0px 0px -99% 0px", // little hack to push the rootMargin up to the top of the page
+  threshold: 0, // how much % of the document.body should be visible for the handler function to fire?
 };
 
 // create the observer and decide the function name + the options for it
-const observer = new window.IntersectionObserver(handler, options)
+const observer = new window.IntersectionObserver(handler, options);
 // give the observer some dom node(s) to keep an eye on
-observer.observe(nav)
+observer.observe(nav);
 
 // pointer event mouse tracking
 
 // buiten het menu klikken moet dit sluiten
 
 // pub listing
-const revealBtn = document.querySelector('button.btn');
-revealBtn.addEventListener('click', ()=>{
-  console.log('fired');
-  
-  publications.classList.toggle('reveal');
-  console.log(publications.classList);
-  
-})
+const revealBtn = document.querySelector("button.btn");
+revealBtn.addEventListener("click", () => {
+  console.log(pubItems);
+  for (let i = 0; i < pubItems.length; i++) {
+    setTimeout(() => {
+      pubItems[i].classList.add('displayed')
+    }, 300 * i);
+  }
+});
