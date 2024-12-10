a7de79fdee9cc90068dd3a6a3b5e860b63faee99
Marcin Magdziak <marcinm@live.nl>
2024-12-10 08:21:58 +0100

    intersection observer
    replaced scrollY with intersectionObserver and added hover on links and 'buttons' (just a simple color fade on both the text and after)

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,40 @@
 const pageWithoutScroll = window.innerWidth - 15;
 const navItems = document.querySelector(".nav-items");
 const nav = document.querySelector("nav");
+const header = document.querySelector("header")
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay");
 let isScrolled = false;
 
-// switch to intersectionObserver
-const checkScroll = () => {
-  const scrolledDown = window.scrollY > 0;
-
-  if (scrolledDown && !isScrolled) {
-    nav.classList.add("scrolled");
-    isScrolled = true;
-  } else if (!scrolledDown && isScrolled) {
-    nav.classList.remove("scrolled");
-    isScrolled = false;
-  }
-};
-
-pageWithoutScroll > 660 && window.addEventListener("scroll", checkScroll) && burgerMenu.addEventListener("click", OpenBurger);
-
 const OpenBurger = (e) => {
-  console.log(navItems);
   e.preventDefault();
   overlay.classList.toggle("open");
   navItems.classList.toggle("open");
 };
 burger.addEventListener("click", OpenBurger);
 
+const handler = (entries) => {
+  // entries is an array of observed dom nodes
+  // we're only interested in the first (and only) one at [0]
+  // because that's my nav node.
+  // Here observe whether or not that node is in the viewport
+  if (!entries[0].isIntersecting) {
+    nav.classList.add('scrolled')
+  } else {
+    nav.classList.remove('scrolled')
+  }
+}
+const options = {
+  root: document.body, // The root of the observer
+  rootMargin: '0px 0px -99% 0px', // little hack to push the rootMargin up to the top of the page
+  threshold: 0 // how much % of the document.body should be visible for the handler function to fire?
+};
+
+// create the observer and decide the function name + the options for it
+const observer = new window.IntersectionObserver(handler, options)
+// give the observer some dom node(s) to keep an eye on
+observer.observe(nav)
+
+// pointer event mouse tracking
+
+// buiten het menu klikken moet dit sluiten
